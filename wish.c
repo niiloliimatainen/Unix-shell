625e42656578eeafd28e06d1abbcfd2f3ada4584
niiloliimatainen <niilo.liimatainen@student.lut.fi>
2021-04-16 18:18:55 +0300

    oskulle pohja

--- wish.c
+++ wish.c
@@ -4,13 +4,39 @@
 
 int main(int argc, char *argv[]) {
 
+    if (argc == 1) {
+        wish();
+    }
 
 
 
 
 
 
-    
 
     return 0;
 }
+
+
+void wish() {
+    while (1) {
+        printf("wish>");
+
+    }
+
+}
+
+void cd() {
+
+}
+
+
+void path() {
+
+}
+
+
+void redirect() {
+
+}
+
